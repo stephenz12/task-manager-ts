e42932a3347f4f758ab59dbaf45e1fca2e9e3f75
Stephen Piperno <hanjkn019@hushmail.com>
2025-12-28 11:24:34 -0500

    Final Auth0 integration and README update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { useTasks } from "./context/TaskContext";
+import { useAuth0 } from "@auth0/auth0-react";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
 function App() {
-  const { tasks } = useTasks();
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   return (
     <div>
       <h1>Task Management App</h1>
 
-      <TaskForm />
+      {!isAuthenticated ? (
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      ) : (
+        <>
+          <button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </button>
 
-      <h2>Tasks</h2>
-      <TaskList />
-
-      <p>Total tasks: {tasks.length}</p>
+          <TaskForm />
+          <TaskList />
+        </>
+      )}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 import { TaskProvider } from "./context/TaskContext";
 
@@ -9,8 +10,16 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <TaskProvider>
-      <App />
-    </TaskProvider>
+    <Auth0Provider
+      domain="dev-8xo6z885argwms1m.us.auth0.com"
+      clientId="tfbsZa6mhkX7hfmmuoDMCGFguI7R2j0e"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <TaskProvider>
+        <App />
+      </TaskProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
